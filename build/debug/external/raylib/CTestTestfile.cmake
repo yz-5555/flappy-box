bbaf3136923488012b460d0f13656e9add68e3c8
yz <yunjae00734@gmail.com>
2024-12-25 02:48:55 +0900

    Update .gitignore

--- build/debug/external/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/devs/projects/flappy-box/external/raylib
-# Build directory: D:/devs/projects/flappy-box/build/debug/external/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
